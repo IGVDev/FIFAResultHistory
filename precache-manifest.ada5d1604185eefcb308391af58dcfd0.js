350f63b4570306ade9805abb7bc8d62f8b45fe89
IGVDev <guidoigv@gmail.com>
2021-09-17 15:50:48 -0300

    Updates

---
+++ precache-manifest.ada5d1604185eefcb308391af58dcfd0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e06e60f968cab2b34ed14b2d90ecda2",
+    "url": "/FIFAResultHistory/index.html"
+  },
+  {
+    "revision": "4886a4d658870f162e66",
+    "url": "/FIFAResultHistory/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ab322f5f4999f1e250f6",
+    "url": "/FIFAResultHistory/static/css/main.0cfc94fe.chunk.css"
+  },
+  {
+    "revision": "4886a4d658870f162e66",
+    "url": "/FIFAResultHistory/static/js/2.8d203ab3.chunk.js"
+  },
+  {
+    "revision": "fe4cdd745462666d252711c3f664a082",
+    "url": "/FIFAResultHistory/static/js/2.8d203ab3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab322f5f4999f1e250f6",
+    "url": "/FIFAResultHistory/static/js/main.2866cf69.chunk.js"
+  },
+  {
+    "revision": "21c5ed29a152acc3bce1",
+    "url": "/FIFAResultHistory/static/js/runtime-main.b2b01345.js"
+  }
+]);
