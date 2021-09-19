29a05776a6d736a8b1222748a3c6be8f15b4b1ea
IGVDev <guidoigv@gmail.com>
2021-09-18 23:04:54 -0300

    enabled match saving

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,34 +7,34 @@ const Navigation = ({
   logout,
   user,
 }) => {
+  // if (isSignedIn) {
+  //   return (
+  //     <nav style={{ display: "flex", justifyContent: "flex-end" }}>
+  //       <p className="f3 white pa3 absolute left-0">Hello {user.name}</p>
+  //       <p
+  //         onClick={() => onRouteChange("home")}
+  //         className="f3 link dim white underline pa3 pointer"
+  //       >
+  //         Home
+  //       </p>
+  //       {/* <p
+  //         onClick={() => onRouteChange("profile")}
+  //         className="f3 link dim white underline pa3 pointer"
+  //       >
+  //         Profile
+  //       </p> */}
+  //       <p onClick={null} className="f3 dark-gray underline pa3">
+  //         Load Match
+  //       </p>
+  //       <p
+  //         onClick={() => logout({ returnTo: window.location.origin })}
+  //         className="f3 link dim white underline pa3 pointer"
+  //       >
+  //         Sign Out
+  //       </p>
+  //     </nav>
+  //   );
   if (isSignedIn) {
-    return (
-      <nav style={{ display: "flex", justifyContent: "flex-end" }}>
-        <p className="f3 white pa3 absolute left-0">Hello {user.name}</p>
-        <p
-          onClick={() => onRouteChange("home")}
-          className="f3 link dim white underline pa3 pointer"
-        >
-          Home
-        </p>
-        {/* <p
-          onClick={() => onRouteChange("profile")}
-          className="f3 link dim white underline pa3 pointer"
-        >
-          Profile
-        </p> */}
-        <p onClick={null} className="f3 dark-gray underline pa3">
-          Load Match
-        </p>
-        <p
-          onClick={() => logout({ returnTo: window.location.origin })}
-          className="f3 link dim white underline pa3 pointer"
-        >
-          Sign Out
-        </p>
-      </nav>
-    );
-  } else if (isSignedIn && user.name === "Guido") {
     return (
       <nav style={{ display: "flex", justifyContent: "flex-end" }}>
         <p className="f3 white pa3 absolute left-0">Hello {user.name}</p>
